42c248573ae66f41ed68ca10e825b5f5abbdbfe0
Roman Aleksandrovich <ceoro9@protonmail.com>
2019-12-22 16:41:14 +0300

    Thread pool worker input

--- thread_pool.c
+++ thread_pool.c
@@ -3,18 +3,66 @@
 #include <pthread.h>
 #include <stdbool.h>
 
+// ThreadPool ...
 typedef struct thread_pool_t_ {
-    bool is_active;
+    
+    // Signifies if pool is opened
+    bool is_opened;
+    
+    // Number of thread workers in pool
     unsigned int number_of_threads;
+
+    // Array of thread worker ids
     pthread_t *thread_ids;
+
 } thread_pool_t;
+
+// ThreadPoolWorkerInput ...
+typedef struct thread_pool_worker_input_ {
+
+    // Signifies if pool is opened
+    bool is_pool_opened;
+    
+    // Barrier to wait until all workers are set up
+    #ifdef _POSIX_BARRIERS
+    pthread_barrier_t *thread_barrier_ptr;
+    #endif
+    
+    // Condition to get a function to execute 
+    pthread_cond_t *thread_cond_ptr;
+    
+    // Mutex to sync access to function
+    pthread_mutex_t *thread_mutex_ptr;
+
+    // Function to execute by one of workers
+    void (*function_to_execute_ptr)(void*);
+
+} thread_pool_worker_input_t;
 
 void thread_pool_worker(void *data) {
 
     #ifdef _POSIX_BARRIERS
 
-    pthread_barrier_t *thread_barrier_ptr = (pthread_barrier_t*) data;
-    pthread_barrier_wait(thread_barrier_ptr);
+    thread_pool_worker_input_t *worker_input = (thread_pool_worker_input_t*) data;
+    
+    pthread_barrier_wait(worker_input->thread_cond_ptr);
+
+    while (worker_input->is_pool_opened) {
+
+	pthread_mutex_lock(worker_input->thread_mutex_ptr);
+	
+	while (worker_input->function_to_execute_ptr == NULL) {
+	    pthread_cond_wait(
+		worker_input->thread_cond_ptr,
+		worker_input->thread_mutex_ptr
+	    );
+	}
+
+	worker_input->function_to_execute_ptr();
+	worker_input->function_to_execute_ptr = NULL;
+
+	pthread_mutex_unlock(worker_input->thread_mutex_ptr);
+    }
 
     #else
 
@@ -22,11 +70,6 @@ void thread_pool_worker(void *data) {
     exit(1);
 
     #endif
-
-    // TODO: while thread_pool is active
-    while (true) {
-
-    }
 } 
 
 thread_pool_t *create_thread_pool(unsigned int number_of_threads) {
@@ -37,7 +80,7 @@ thread_pool_t *create_thread_pool(unsigned int number_of_threads) {
         return NULL;
     }
 
-    result_thread_pool->is_active = true;
+    result_thread_pool->is_opened = true;
     result_thread_pool->number_of_threads = number_of_threads;
     result_thread_pool->thread_ids = (pthread_t*) malloc(sizeof(pthread_t) * number_of_threads);
 
@@ -70,7 +113,7 @@ thread_pool_t *create_thread_pool(unsigned int number_of_threads) {
 }
 
 void destroy_thread_pool(thread_pool_t *thread_pool) {
-    thread_pool->is_active = false;
+    thread_pool->is_opened = false;
     // TODO: wait until all thread are done
     free(thread_pool->thread_ids);
     free(thread_pool);
